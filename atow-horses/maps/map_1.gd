1f77f22d6bd97b4c71a396e834712f6f28e09076
WilliamA-bar <121774129+WilliamAalund@users.noreply.github.com>
2026-04-17 19:51:05 -0500

    Horses spawn now

--- atow-horses/maps/map_1.gd
+++ atow-horses/maps/map_1.gd
@@ -1,12 +1,24 @@
 extends Node2D
 
+const horse := preload("res://Horse.tscn")
+
 @export var map: Polygon2D
 @export var collision: CollisionPolygon2D
+@export var spawns: Node
 
 func _ready() -> void:
-	assert(map != null and collision != null)
+	assert(map != null and collision != null and spawns != null)
 	collision.polygon = map.polygon
 	collision.position = map.position
+	spawn_horses(2)
 
-func spawn_horses() -> void:
-	pass # TODO
+func spawn_horses(num_horses: int) -> void:
+	var horse_spawns := spawns.get_children()
+	
+	assert(horse_spawns.size() >= num_horses)
+	
+	# This assumes that all children of spawns are Node2Ds
+	for i in range(num_horses):
+		var new_horse = horse.instantiate()
+		new_horse.global_position = horse_spawns[i].global_position
+		add_child(new_horse)
