2a5766f315cc81df83b30742a2e7849e4075874c
Sofia Cantu Garza <sofia17cantu@gmail.com>
2026-04-20 20:56:42 -0500

    added more randomization to horse's movement

--- atow-horses/horses/horse.gd
+++ atow-horses/horses/horse.gd
@@ -1,12 +1,14 @@
 extends CharacterBody2D
 
-
+@onready var horse_sprite: = $Sprite2D
 var direction: Vector2 = Vector2(0.5, 0.5).normalized()
-var speed: int = 90;
+var speed: int = 150;
 
 func _ready() -> void:
 	# start with a random normalized direction vector
-	direction = Vector2.from_angle(randf_range(0, TAU))
+	horse_sprite.modulate = Color(randf_range(0, 1),randf_range(0, 1),randf_range(0, 1),1)
+	print(horse_sprite.modulate)
+	direction = Vector2.from_angle(randf_range(0, TAU)).normalized()
 
 func _physics_process(delta: float) -> void:
 	velocity = direction * speed
@@ -18,8 +20,10 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func handle_collision(collision):
-	# get new direction based off of collison angle
-	var normal  = collision.get_normal()
+	# get new direction based off of collison angle with a random offset
+	var offset_val = 0.2
+	var offset = Vector2(randf_range(-offset_val, offset_val), randf_range(-offset_val, offset_val))
+	var normal = (collision.get_normal() + offset).normalized()
 	direction = direction.bounce(normal)
 	# or get new direction randomly
 	#direction = Vector2.from_angle(randf_range(0, TAU))
--- atow-horses/seed.gd
+++ atow-horses/seed.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var global_seed = "aotw-horses"
+var global_seed = "will's-horse"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
