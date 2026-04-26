1146c6148a6fa4b3867f19e6df1fc0eada26d71d
nickmp4 <nicholassheninger@gmail.com>
2026-04-25 21:20:38 -0500

    added horse names, shown at beginning, victory label displays proper names

--- atow-horses/ui/victory_screen.gd
+++ atow-horses/ui/victory_screen.gd
@@ -1,19 +1,21 @@
 extends Control
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var winner_name = $Label
 
 func _ready() -> void:
 	GameSignals.to_main.connect(_on_to_main)
 	GameSignals.finish_game.connect(_on_finish_game)
 
-func play_victory_screen() -> void:
+func play_victory_screen(horse_name: String) -> void:
+	winner_name.text = horse_name + " won!"
 	animation_player.play("begin_victory")
 	await animation_player.animation_finished
 	animation_player.play("victory")
 	await animation_player.animation_finished
 
-func _on_finish_game() -> void:
-	play_victory_screen()
+func _on_finish_game(horse_name: String) -> void:
+	play_victory_screen(horse_name)
 
 func _on_to_main() -> void:
 	animation_player.stop()
